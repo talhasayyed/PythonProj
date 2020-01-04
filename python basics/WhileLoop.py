31fa6b7d77b66c84533bd79304ac71dcc30369a5
tattv <talhasay2u@gmail.com>
2020-01-04 16:09:46 +0530

    while loop

---
+++ python basics/WhileLoop.py
@@ -0,0 +1,13 @@
+
+# initialization
+i = 1
+while i<=5: # condition
+    print("Talha",end="")
+    j=1
+    while j<5:
+        print("Rocks",end="")
+        j += 1
+    i += 1  # increment
+    print()
+
+

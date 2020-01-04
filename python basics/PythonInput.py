1050dab503b79f7c3f3ea18e9cc376e16d0b72b4
tattv <talhasay2u@gmail.com>
2020-01-04 15:45:16 +0530

    input in system terminal

---
+++ python basics/PythonInput.py
@@ -0,0 +1,11 @@
+
+# string inputs
+x = input('string is ')
+print(x)
+print()
+
+# char input
+y = input("enter char ")[0]
+print(y)
+
+# int input
---
+++ python basics/python_system_inputs.py
@@ -0,0 +1,7 @@
+import sys
+
+# cd python basics
+# python python_system_inputs.py 2
+a = sys.argv[1]
+print(a," is sting input")
+

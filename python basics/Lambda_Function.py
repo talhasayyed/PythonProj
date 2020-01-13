afabff73d3db96b8c652ff59329281689eea7392
tattv <talhasay2u@gmail.com>
2020-01-13 12:25:29 +0530

    Lamda Function to find square

---
+++ python basics/Lambda_Function.py
@@ -0,0 +1,6 @@
+# 42
+# Lamda Function to find square
+
+f = lambda n : n*n
+result = f(5)
+print(result)
--- test.py
+++ test.py
@@ -1,27 +1,11 @@
-# python fibonacci
+# 41 fact using recursion
 
-fibonacci = int(input("enter number for fibonacci: "))
-
-i=1 #index
-while fibonacci >= 0:
-
-    a = 0
-    b = 1
-
-    if fibonacci == 0:
-        print(i,"index",fibonacci)
-    else:
-        print(i,"index",0)
-        i+=1 # index number increment
-        print(i,"index",1)
-    for i in range(2,fibonacci):
-        i+=1 # index number increment
-        c = a + b
-        a = b
-        b = c
-        print(i,"index",c)
-
-    print("bye")
-    break
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n - 1)
 
 
+x = 5
+result = fact(x)
+print(result)

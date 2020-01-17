b7d66efc50c6fc7680610183d6a1e622bf82fef0
tattv <talhasay2u@gmail.com>
2020-01-17 19:04:46 +0530

    Duck Typing ( Polymorphism )

--- python basics/DuckTyping.py
+++ python basics/DuckTyping.py
@@ -1,12 +1,13 @@
 class Pycharm:
     def execute(self):
-        print('compile')
+        print('compiler start')
 
 class Laptop:
     def code(self,ide):
+        print('laptop start')
         ide.execute()
 
 ide = Pycharm()
 
 l1 = Laptop()
-l1.execute(ide)
+l1.code(ide)

c6eac9ed22c873c30c6276ee05c767d4d0dea761
Talha <talhasay2u@gmail.com>
2020-01-19 18:48:40 +0530

    Iterator for memory management in loop

---
+++ python basics/Iterator.py
@@ -0,0 +1,24 @@
+# Iterator for memory management in loop
+# is used in conditon where u dont want to store all values in memory
+# just want one value in memory
+
+
+class TopTen:
+    def __init__(self):
+        self.num = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num <=20:
+            vals = self.num
+            self.num += 1
+            return vals
+        else:
+            raise StopIteration
+
+Values = TopTen()
+
+for i in Values:
+    print(i)

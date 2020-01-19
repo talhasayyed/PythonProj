28c7e6ad007f42bf1d59c9b35384eb9266f23427
Talha <talhasay2u@gmail.com>
2020-01-19 18:57:44 +0530

    Iterator for memory management in loop

---
+++ python basics/Generator.py
@@ -0,0 +1,14 @@
+# Generator using yield
+
+def topten():
+    # This is not possible with return
+    yield 1
+    yield 2
+    yield 3
+
+value = topten()
+
+print(value.__next__())
+# or
+print(next(value))
+print(next(value))

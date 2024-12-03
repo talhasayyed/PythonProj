061f5fcabd169a6228621f0959abbbd610d3938c
Talha Sayyed <talhasay2u@gmail.com>
2024-12-04 01:44:58 +0530

    check time function for Fibonacci recursive and optimised.

--- python basics/Qustion/check_time_of_func.py
+++ python basics/Qustion/check_time_of_func.py
@@ -1,6 +1,9 @@
 from typing import Callable 
 import time
 
+from febonacci_optimised import febonacci
+from febonacci_recursion import fibonacci_recursive
+
 def time_function(func: Callable [[int], int], n: int) -> None:
     start_time: float = time.perf_counter()
     result: int = func(n)
@@ -8,3 +11,10 @@ def time_function(func: Callable [[int], int], n: int) -> None:
     # Print information
     print(f'{func.___name__}({n})={result}')
     print(f'Total time: {end_time - start_time:.4f}s')
+
+
+# print febonacci_recursive time
+print(time_function(func: febonacci_recursive, n:10))
+
+# print febonacci optimised time
+print(time_function(func: febonacci, n:10))

2f707563f136b653f58fa36c6f6dba5166cb5499
Talha Sayyed <talhasay2u@gmail.com>
2024-12-04 01:15:38 +0530

    Update LeapYear.py

--- python basics/Qustion/LeapYear.py
+++ python basics/Qustion/LeapYear.py
@@ -2,14 +2,17 @@
 
 def leapyear(year):
     leap = False
+    # year divisible by 400
     if year % 400 == 0:
         leap= True
+    # year not divisible by 100
     elif year % 100 != 0:
         leap= False
+    # year divisible by 4
     elif year % 4 == 0:
         leap= True
     return leap
 
 
-yr = int(input("enter year "))
+yr = int(input("enter year: "))
 print(leapyear(yr))
